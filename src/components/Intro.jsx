81a9737293d1c9425a69b5eb34e1a5b6757335b0
clarkjohn000112 <clarktoledana01@gmail.com>
2023-08-28 19:42:01 +0800

    Initial Commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Intro() {
+   return (
+      <div className="flex items-center justify-center flex-col text-center pt-20 pb-1">
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Clark John Casin</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">Quality Assurance Engineer & Web Developer</p>
+         <p className="text-sm max-w-xl mb-6 font-normal">
+         I'm Clark John Casin, a recent Computer Science graduate with a deep passion for front-end web development. As an active member of a college software dev team, I've honed my collaboration skills. Currently interning as a Quality Assurance Engineer at Achieve Without Borders Inc., I'm excited to shift my career towards front-end development. Thanks for reviewing my portfolio!
+         </p>
+      </div>
+   )
+}
+
+export default Intro;
---
+++ src/data/timeline.js
@@ -0,0 +1,37 @@
+export default [
+   {
+      year: 'Today',
+      title: 'Job Hunting',
+      duration: '1 Month',
+      details: 
+         'I am currently looking for front end developer position preferably an entry level position to gain more knowledge and improve my skills.',
+   },
+   {
+		year: '2023',
+		title: 'Bachelor of Science in Computer Science',
+		duration: '4 years',
+		details:
+			'I embarked on a pursuit of knowledge within the realm of computer science. This journey led me through computer architecture, theory, diverse programming languages, and elements of artificial intelligence.',
+	},
+	{
+		year: '2023',
+		title: 'Quality Assurance Freelance',
+		duration: '6 Months',
+		details:
+			'My company for the internship offered me to work full time or freelance in the same position but as an addition I will be assigned into the Automation Testing Team of the HRIS Development Team.',
+	},
+	{
+		year: '2022',
+		title: "Quality Assurance Intern",
+		duration: '3 Months',
+		details:
+			'I started my Intern on December 15, 2022, I applied for Quality Assurance Engineer at Achieve Without Borders Inc., I am assigned into an HRIS team in which my role is to test HRIS System for the client',
+	},
+	{
+		year: '2017',
+		title: 'Start of the journey',
+		duration: 'Introduction to coding',
+		details:
+			"This year stands out as a pivotal moment in my journey. My foray into coding began during my Senior High School year when I chose the IT strand. I delved into Java initially, swiftly followed by mastering the art of crafting websites using HTML, CSS, and Javascript. ",
+	},
+]
